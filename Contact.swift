fe8d34f92bfeff06570b94d54a4445cf92f6267e
rvcstudent1 <webstudent@PC16286.local>
2015-10-26 16:20:20 -0500

    A.G.

---
+++ Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  MyCoreData
+//
+//  Created by webstudent on 10/26/15.
+//  Copyright © 2015 webstudent. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
